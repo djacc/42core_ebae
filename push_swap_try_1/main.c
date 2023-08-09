010305efeaea884444227c3e2056590a56d39b3a
eddv <116167145+djacc@users.noreply.github.com>
2023-08-09 12:45:41 +0200

    Add files via upload

---
+++ push_swap_try_1/main.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebae <eddybae@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/08 19:07:04 by eddybae           #+#    #+#             */
+/*   Updated: 2023/08/08 19:07:04 by eddybae          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	main(int argc, char **argv)
+{
+	t_list	*stack_a;
+	t_list	*stack_b;
+
+	*stack_a = NULL;
+	*stack_b = NULL;
+
+	if (argc < 2)
+		return (1);
+
+	// check if the arguments are valid
+
+	// initialize stack_a and stack_b, 
+	// stack_a should be initialized with the arguments
+
+	//
+
+}
